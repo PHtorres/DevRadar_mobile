df3c3d9e36e54484f185babe866f772c85b20c78
PHTorres <tghpfbc@gmail.com>
2020-02-03 18:12:06 -0300

    mapa no app

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,48 @@
-import React from 'react';
-import { View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-function Main(){
-    return (<View/>);
+function Main() {
+
+    const [regiaoCorrente, setRegiaoCorrente] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialLocation() {
+            const { granted } = await requestPermissionsAsync();
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true
+                });
+
+                const { latitude, longitude } = coords;
+
+                setRegiaoCorrente({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.005,
+                    longitudeDelta: 0.005,
+                });
+            }
+        }
+
+        loadInitialLocation();
+    }, []);
+
+    if(!regiaoCorrente){
+        return null;
+    }
+
+
+    return (
+        <MapView initialRegion={regiaoCorrente} style={styles.map} />
+    );
 }
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    }
+});
 
 export default Main;
